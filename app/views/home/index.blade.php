a3aad4701e84b40bcbc6248a8307612b9e15e10a
Purinda Gunasekara <purinda@gmail.com>
2014-09-21 19:14:17 +1000

    * button captions updated.

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -34,13 +34,13 @@
         <form enctype="multipart/form-data" role="form" action="/upload" method="post">
           <div class="fileinput fileinput-new input-group" data-provides="fileinput">
             <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
-            <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select file</span><span class="fileinput-exists">Change</span><input type="file" name="ingredients-file"></span>
+            <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select Ingredients</span><span class="fileinput-exists">Change</span><input type="file" name="ingredients-file"></span>
             <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
           </div>
 
           <div class="fileinput fileinput-new input-group" data-provides="fileinput">
             <div class="form-control" data-trigger="fileinput"><i class="glyphicon glyphicon-file fileinput-exists"></i> <span class="fileinput-filename"></span></div>
-            <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select file</span><span class="fileinput-exists">Change</span><input type="file" name="recipes-file"></span>
+            <span class="input-group-addon btn btn-default btn-file"><span class="fileinput-new">Select Recipes</span><span class="fileinput-exists">Change</span><input type="file" name="recipes-file"></span>
             <a href="#" class="input-group-addon btn btn-default fileinput-exists" data-dismiss="fileinput">Remove</a>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
